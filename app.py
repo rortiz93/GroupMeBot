e3d2bb683fedcb1d5230e235221f6d45109bca3d
Ryan <ortizryan76@yahoo.com>
2017-08-30 18:20:56 -0400

    updates to add /score

--- app.py
+++ app.py
@@ -14,25 +14,18 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
-  print("x")
+
 
   # We don't want to reply to ourselves!
   if data['name'] != 'bitch nutz':
-    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    GroupMeBot.send_message(full_text)
+      if data['text'] == '/score':
+          GroupMeBot.send_message(full_text)
+
+
 
   return "ok", 200
 
 
-def send_message(msg):
-    url = 'https://api.groupme.com/v3/bots/post'
-
-    data = {
-        'bot_id': os.getenv('botId'),
-        'text': msg,
-    }
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
 
 
 def log(msg):
