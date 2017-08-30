79be62b9ca52804b53c74f1c87ec926472188f49
Ryan <ortizryan76@yahoo.com>
2017-08-30 14:44:50 -0400

    af

--- Listener.py
+++ Listener.py
@@ -1,5 +1,8 @@
 import os
 import json
+
+import sys
+
 import GroupMeBot
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -15,6 +18,22 @@ def webhook():
   # We don't want to reply to ourselves!
   if data['name'] != 'bitch nutz':
     msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    GroupMeBot.send_message(full_text)
+    send_message(msg)
 
   return "ok", 200
+
+
+def send_message(msg):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    data = {
+        'bot_id': os.getenv('GROUPME_BOT_ID'),
+        'text': msg,
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+
+
+def log(msg):
+    print(str(msg))
+    sys.stdout.flush()
